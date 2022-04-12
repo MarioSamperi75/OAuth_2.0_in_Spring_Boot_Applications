879df578b5f3b69018cd6b6d5efd8881143b7d8c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-12 15:54:26 +0200

    Resource Server: getting properties from JWT, the user ID - advanced work with @PreAnnotation

--- ResourceServer -Method level Security/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ ResourceServer -Method level Security/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +20,15 @@ public class UsersController {
 	}
 	
 	//@Secured("ROLE_developer")
-	// @PreAuthorize allows to use method security expression - more powerful!!
+	//@PreAuthorize allows to use method security expression - more powerful!!¨
+	// we can inject the Jwt object (@A@AuthenticationPrincipal)
+	// We can compare the path variable with the property sub (subject, user) in the Jwt object
+	
+	@PreAuthorize("hasRole('developer') or #id == #jwt.subject")
 	//@PreAuthorize("hasAuthority('ROLE_developer')")
-	@PreAuthorize("hasRole('developer')")
 	@DeleteMapping(path = "/{id}")
-	public String deleteUser (@PathVariable String id) {
-		return "Deleted user with id: " + id;
+	public String deleteUser (@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return "Deleted user with id: " + id + "and JWT subject" + jwt.getSubject();
 	}
 	
 }
