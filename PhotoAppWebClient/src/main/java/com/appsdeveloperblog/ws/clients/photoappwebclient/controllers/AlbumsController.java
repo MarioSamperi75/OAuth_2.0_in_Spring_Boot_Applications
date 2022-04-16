07abe78ad7dbad7da5fb0d251c4723185a67e37a
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-16 21:51:58 +0200

    MVC_Web_App: working with the model

--- PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -1,14 +1,40 @@
 package com.appsdeveloperblog.ws.clients.photoappwebclient.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.appsdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 
 @Controller
 public class AlbumsController {
 	
 	@GetMapping("/albums")
 	public String getAlbums (Model model) {		
+		
+		AlbumRest album = new AlbumRest();
+		album.setAlbumId("albumTwo");
+		album.setAlbumTitle("Album Two Title");
+		album.setAlbumUrl("http://localhost:8082/albums/1");
+		
+		AlbumRest album2 = new AlbumRest();
+		album2.setAlbumId("albumOne");
+		album2.setAlbumTitle("Album One Title");
+		album2.setAlbumUrl("http://localhost:8082/albums/1");
+		
+		List returnValue = Arrays.asList(album, album2 );
+		
+		//to have the list available in the html page we have to add it to the model 
+		model.addAttribute("albums", returnValue);
+		
+		
+		
+		
+		
+		
 		// we return the name of the view
 		return "albums";
 	}
