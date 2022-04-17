bdec8c730feadd9552c4933ab5c531735516e348
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-17 21:00:23 +0200

    MVC_Web_App: reading the JWT access token

--- PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -3,7 +3,13 @@ package com.appsdeveloperblog.ws.clients.photoappwebclient.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -15,9 +21,37 @@ import com.appsdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 @Controller
 public class AlbumsController {
 	
+	// to get the JWT access token we need the 1) OAuthclientService (we can just inject it!)
+	// and the 2) authentication token
+	
+	// #1
+	@Autowired
+	OAuth2AuthorizedClientService oauth2ClientService;
+	
 	@GetMapping("/albums")
-	//Oidcuser object:  all the info about the open id connect provider
-	public String getAlbums (Model model, @AuthenticationPrincipal OidcUser principal) {		
+	// Oidcuser object:  all the info about the open id connect provider
+	// You can get the authentication token from the authentication object
+	// or from the SecurityContextHolder
+	
+	//#2
+	public String getAlbums (Model model, @AuthenticationPrincipal OidcUser principal, 
+			Authentication authentication) {	
+		// #2 alternative
+		//Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		// we just cast it to OAuth2AuthenticationToken
+		OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+		
+		// now we have 1 and 2 ,we can get the JWC access token
+		// we have to get an object of Outh2AutorizedClient, by adding some information from the oauthToken, 
+		// and from this object we can get the JWC access token
+		
+		OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName()); 
+		String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+		
+		System.out.println("JWT Access Token: " +  jwtAccessToken);
+		
+		
 		
 		System.out.println("Principal: " + principal);
 		OidcIdToken idToken = principal.getIdToken();
