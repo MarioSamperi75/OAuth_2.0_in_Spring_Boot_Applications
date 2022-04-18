05864c4c328193d625ba846693e8f718c71aa583
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-18 09:54:14 +0200

    MVC_Web_App: creating the WebClient bean

--- PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
+++ PhotoAppWebClient/src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
@@ -3,7 +3,12 @@ package com.appsdeveloperblog.ws.clients.photoappwebclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PhotoAppWebClientApplication {
@@ -15,6 +20,22 @@ public class PhotoAppWebClientApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	
+	// to automatically detect the client configuration we need, due to the current authentication object
+	// an access token will be included into every single request
+	@Bean
+	public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository, 
+			OAuth2AuthorizedClientRepository oauth2AuthorizedClientRepository) {
+		
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = 
+				new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, 
+						oauth2AuthorizedClientRepository);
+		
+		oauth2.setDefaultOAuth2AuthorizedClient(true);
+		
+		return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
 	}
 
 }
